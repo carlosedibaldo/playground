ccd1822804c1051b434077c698cf0880fd654c3b
Carlos E. Silva <carlosedibaldo@gmail.com>
2026-04-14 11:07:09 -0400

    initial dclab commit

---
+++ dclab/config.py
@@ -0,0 +1,15 @@
+ROOT_DIR = "/Users/carlossilva/Downloads/dclab-data/"
+CSV_PATHS = {
+    "crime": f"{ROOT_DIR}/csv/crime_2026.csv",
+    "stops": f"{ROOT_DIR}/csv/stops_2026.csv",
+    "income": f"{ROOT_DIR}/csv/IncomeData.csv",
+    "courts": f"{ROOT_DIR}/csv/CourtsData1.csv",
+    "appraisal": f"{ROOT_DIR}/csv/RESIDENTIAL_CAMA.csv"
+}
+
+GEOJSON_PATHS = {
+    "address_points": f"{ROOT_DIR}/geojson/Address_Points.geojson",
+    "bike_lanes": f"{ROOT_DIR}/geojson/Bicycle_Lanes.geojson",
+    "bike_share_locations": f"{ROOT_DIR}/geojson/Capital_Bike_Share_Locations.geojson",
+    "housing_characteristics": f"{ROOT_DIR}/geojson/Housing_Characteristics_DC_Census_Tracts.geojson",
+}
